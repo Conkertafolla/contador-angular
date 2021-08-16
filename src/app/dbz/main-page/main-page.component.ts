2c86cb9d7f90c8e3830b0e38732601b85375555c
Gerardo Tafolla <conkertafolla2.0@gmail.com>
2021-08-15 23:27:09 -0500

    Implement services

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 
 @Component({
@@ -8,21 +9,15 @@ import { Personaje } from '../interfaces/dbz.interface';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent  {
-  personajes:Personaje[]=[
-    {nombre:"Goku",poder:15000},
-    {nombre:"Vegeta",poder:8500}
-  ]
+  
 
   nuevo :Personaje={
     nombre:"Krilin",
     poder:80
   }
-
-  agregarNuevoPersonaje(argumento:Personaje):void{
-    this.personajes.push(argumento)
+  
+  constructor(){
     
-
   }
-
  
 }
